f87f6e84ed6bd7639adb59bcafff3754d3fce5ee
violetasicilia4 <104385265+violetasicilia4@users.noreply.github.com>
2022-10-20 23:52:19 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,22 @@
-from django.urls import path
-from familia import views
+"""mvt URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('agregar/', views.agregar, name="agregar"),
-    path('borrar/<identificador>', views.borrar, name="borrar"),
-    path('actualizar/', views.actualizar, name="actualizar_action"),
-    path('actualizar/<identificador>', views.actualizar, name="actualizar"),
-    path('buscar/', views.buscar, name="buscar"),
+    path('', include('familia.urls')),
+    path('admin/', admin.site.urls),
 ]
